0dd7b32c23b486bf1c3797a7a0a66f13fc88316f
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-05-24 23:04:30 +0100

    Updated SQL with new schema and added john n sofia to db

--- backing-services/mysql/data/init.sql
+++ backing-services/mysql/data/init.sql
@@ -2,14 +2,6 @@ CREATE TABLE `users` (
     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `uid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
     `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-    `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-    `gid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-    `home_directory` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-    `uid_number` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-    `student_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-    `course` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-    `graduation_year` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-    `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
     `created_at` timestamp NOT NULL DEFAULT NOW(),
     `updated_at` timestamp NOT NULL DEFAULT NOW(),
     `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -20,12 +12,14 @@ CREATE TABLE `users` (
 CREATE USER 'john'@'%' IDENTIFIED BY 'johns-password';
 CREATE USER 'sofia'@'%' IDENTIFIED BY 'sofias-password';
 
+INSERT INTO users (`uid`, `name`, `email`) VALUES ('john', 'John Jordan', '112233445@umail.ucc.ie');
+INSERT INTO users (`uid`, `name`, `email`) VALUES ('sofia', 'Sofia Bosak', '544332211@umail.ucc.ie');
 GRANT
     SELECT, INSERT, UPDATE, DELETE,
     CREATE, DROP, REFERENCES, INDEX,
     ALTER, EXECUTE, CREATE ROUTINE, ALTER ROUTINE 
 ON
-    `%`.* TO 'john'@'%';
+    *.* TO 'john'@'%';
 
 GRANT
     SELECT, INSERT, UPDATE, DELETE,
