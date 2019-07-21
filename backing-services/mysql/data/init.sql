4a49dc2e0f388faf51e69ba6bf2a9bf9cda1854d
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2019-07-21 18:37:42 +0100

    Made it possible for mysql stuff to be done on startup without recreating image

--- backing-services/mysql/data/init.sql
+++ backing-services/mysql/data/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `users` (
+CREATE TABLE `users` IF NOT EXISTS (
     `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `uid` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
     `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -9,11 +9,12 @@ CREATE TABLE `users` (
     UNIQUE KEY `users_uid_unique` (`uid`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
 
-CREATE USER 'john'@'%' IDENTIFIED BY 'johns-password';
-CREATE USER 'sofia'@'%' IDENTIFIED BY 'sofias-password';
+CREATE USER IF NOT EXISTS 'john'@'%' IDENTIFIED BY 'johns-password';
+CREATE USER IF NOT EXISTS 'sofia'@'%' IDENTIFIED BY 'sofias-password';
 
-INSERT INTO users (`uid`, `name`, `email`) VALUES ('john', 'John Jordan', '112233445@umail.ucc.ie');
-INSERT INTO users (`uid`, `name`, `email`) VALUES ('sofia', 'Sofia Bosak', '544332211@umail.ucc.ie');
+INSERT INTO users (`uid`, `name`, `email`) VALUES ('john', 'John Jordan', '112233445@umail.ucc.ie') ON DUPLICATE KEY UPDATE;
+INSERT INTO users (`uid`, `name`, `email`) VALUES ('sofia', 'Sofia Bosak', '544332211@umail.ucc.ie') ON DUPLICATE KEY UPDATE;
+
 GRANT
     SELECT, INSERT, UPDATE, DELETE,
     CREATE, DROP, REFERENCES, INDEX,
@@ -26,4 +27,4 @@ GRANT
     CREATE, DROP, REFERENCES, INDEX,
     ALTER, EXECUTE, CREATE ROUTINE, ALTER ROUTINE 
 ON
-    `sofia\_%`.* TO 'sofia'@'%';      
+    `sofia\_%`.* TO 'sofia'@'%';
