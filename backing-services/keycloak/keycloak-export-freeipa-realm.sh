72acfcb70820a38caa7bc0334a70a59b2bb7615c
Eric Moynihan <git@moynihan.io>
2020-09-08 21:26:30 +0100

    Fix hostname

--- backing-services/keycloak/keycloak-export-freeipa-realm.sh
+++ backing-services/keycloak/keycloak-export-freeipa-realm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker exec -it nsa3_keycloak.netsoc.local_1 /opt/jboss/keycloak/bin/standalone.sh \
+docker exec -it keycloak.netsoc.local /opt/jboss/keycloak/bin/standalone.sh \
 -Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export \
 -Dkeycloak.migration.provider=singleFile \
 -Dkeycloak.migration.realmName=freeipa \
