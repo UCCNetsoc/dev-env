ab49393144d93e10f692b910fc9625368945c875
Eric Moynihan <git@moynihan.io>
2021-10-23 20:56:58 +0100

    Freeipa still borked, everything else seems fine

--- browser-session.sh
+++ browser-session.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if command -v chromium &> /dev/null
 then
-	chromium --proxy-server="socks5://localhost:1080" --incognito --user-data-dir=/tmp
+	chromium --proxy-server="socks5://localhost:1080" --incognito --user-data-dir=/tmp --no-first-run
 else 
-	google-chrome-stable --proxy-server="socks5://localhost:1080" --incognito --user-data-dir=/tmp
+	google-chrome-stable --proxy-server="socks5://localhost:1080" --incognito --user-data-dir=/tmp --no-first-run
 fi
