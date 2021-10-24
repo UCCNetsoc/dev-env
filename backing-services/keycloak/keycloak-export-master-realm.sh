c5855d31708821643e7466ca78f3c0b1146a39f8
Eric Moynihan <git@moynihan.io>
2021-10-24 17:31:10 +0100

    Keycloak setup with openldap

--- backing-services/keycloak/keycloak-export-master-realm.sh
+++ backing-services/keycloak/keycloak-export-master-realm.sh
@@ -2,6 +2,6 @@
 docker exec -it keycloak.netsoc.test /opt/jboss/keycloak/bin/standalone.sh \
 -Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export \
 -Dkeycloak.migration.provider=singleFile \
--Dkeycloak.migration.realmName=freeipa \
+-Dkeycloak.migration.realmName=ldap \
 -Dkeycloak.migration.usersExportStrategy=REALM_FILE \
--Dkeycloak.migration.file=/tmp/keycloak/freeipa-realm-exported.json
+-Dkeycloak.migration.file=/tmp/keycloak/ldap-realm-exported.json
